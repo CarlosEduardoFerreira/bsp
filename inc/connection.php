49132f6d7e8ffcece55ea56b0d7d4798adc512a7
Carlos <ferreira.carlos.e@gmail.com>
2018-10-08 17:08:34 -0400

    update DB info

--- inc/connection.php
+++ inc/connection.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
 //mysqli_select_db($conexao,"bsp") or die(mysqli_error($conexao));
 //mysqli_set_charset($conexao, 'utf8'); 
 
- $host = "wordpress.c60mnkwusy5s.sa-east-1.rds.amazonaws.com:3306";
+$host = "wordpress.c60mnkwusy5s.sa-east-1.rds.amazonaws.com:3306";
 $user = "root";
 $pass = "Nfcer00tp155";
 $conexao = mysqli_connect($host, $user, $pass) or die(mysqli_error());
